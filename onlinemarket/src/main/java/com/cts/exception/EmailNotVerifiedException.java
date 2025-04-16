75e36068c82b447bad7aa1a94564c247bfea174b
Mondal <2389377@cognizant.com>
2025-04-16 11:49:33 +0530

    final

--- onlinemarket/src/main/java/com/cts/exception/EmailNotVerifiedException.java
+++
@@ -1,7 +0,0 @@
-package com.cts.exception;
-
-public class EmailNotVerifiedException extends RuntimeException {
-    public EmailNotVerifiedException(String message) {
-        super(message);
-    }
-}
